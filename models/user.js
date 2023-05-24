190d54363dfb9576dac4579d6a4b34ceab27962d
Andrew Smith <meuzishun@gmail.com>
2023-05-23 21:23:26 -0400

    Create user model

---
+++ models/user.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+// Find out if this works instead:
+// const Schema = require('mongoose').Schema;
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  // password: {
+  //   type: String,
+  //   required: true,
+  // },
+  hashedPassword: {
+    type: String,
+    required: true,
+  },
+  salt: {
+    type: String,
+    required: true,
+  },
+  isAdmin: {
+    type: Boolean,
+    required: true,
+  },
+});
+
+UserSchema.virtual('url').get(function () {
+  return `users/${this._id}`;
+});
+
+module.exports = mongoose.model('User', UserSchema);
