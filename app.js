e79220f9bf1cf2ffb21e916e7ea464cc37bf2a10
Andrew Smith <meuzishun@gmail.com>
2023-05-24 09:07:40 -0400

    Add json and urlencoding middleware

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
 app.listen(process.env.PORT, () =>
   console.log(`Server listening on port ${process.env.PORT}`)
 );
