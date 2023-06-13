4f700829a50b3839f3b8385918e2e4f91d2a1ec4
Andrew Smith <meuzishun@gmail.com>
2023-06-13 16:06:27 -0400

    Clean up app.js and refactor error handler

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ require('./config/database');
 const express = require('express');
 const cors = require('cors');
 const passport = require('passport');
-// const { isAuth, isAdmin } = require('./config/passport');
 const routes = require('./routes/index');
 
 const app = express();
@@ -12,23 +11,16 @@ app.use(cors());
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-// app.use(isAuth);
-// app.use(isAdmin);
-
 app.use(routes);
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  // res.locals.message = err.message;
-  // res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  // res.render('error', { user: req.user });
-  console.log('Error handler called');
-  // console.log(err);
-  res.json(err);
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === 'production' ? null : err.stack,
+  });
 });
 
 app.listen(process.env.PORT, () =>
