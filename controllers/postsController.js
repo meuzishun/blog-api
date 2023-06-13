b8dcd5f77940be8d24cde6cc705389181901a5e2
Andrew Smith <meuzishun@gmail.com>
2023-06-13 16:22:31 -0400

    Refactor postsController to make use of expressAsyncHandler

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -1,67 +1,60 @@
+const asyncHandler = require('express-async-handler');
 const Post = require('../models/post');
 
-const getAllPosts = async (req, res) => {
-  try {
-    const posts = await Post.find({}).populate('author');
+// @desc    Get all posts
+// @route   GET /posts
+// @access  Public
+const getAllPosts = asyncHandler(async (req, res) => {
+  const posts = await Post.find({}).populate('author');
 
-    return res.status(200).json({
-      user: req.user,
-      msg: 'returning all posts...',
-      posts,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  return res.status(201).json({
+    msg: 'returning all posts...',
+    posts,
+  });
+});
 
-const getSinglePost = async (req, res, next) => {
-  try {
-    const post = await Post.findOne({ _id: req.params.postId }).populate(
-      'author'
-    );
+// @desc    Get single post
+// @route   GET /posts/:id
+// @access  Public
+const getSinglePost = asyncHandler(async (req, res) => {
+  const post = await Post.findOne({ _id: req.params.postId }).populate(
+    'author'
+  );
 
-    return res.status(200).json({
-      user: req.user,
-      msg: `returning post ${req.params.postId}...`,
-      post,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  return res.status(200).json({
+    msg: `returning post ${req.params.postId}...`,
+    post,
+  });
+});
 
-const submitNewPost = async (req, res, next) => {
-  try {
-    const post = new Post({
-      author: req.user._id,
-      title: req.body.title,
-      content: req.body.content,
-      timestamp: Date.now(),
-      isPublished: true,
-    });
+// @desc    Post single post
+// @route   POST /posts
+// @access  Private
+const submitNewPost = asyncHandler(async (req, res) => {
+  const post = await Post.create({
+    author: req.user._id,
+    title: req.body.title,
+    content: req.body.content,
+    timestamp: Date.now(),
+    isPublished: true,
+  });
 
-    await post.save();
+  return res.status(200).json({
+    msg: 'submitting new post...',
+    post,
+  });
+});
 
-    return res.status(200).json({
-      msg: 'submitting new post...',
-      post,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+// @desc    Delete single post
+// @route   DELETE /posts
+// @access  Private
+const deleteSinglePost = asyncHandler(async (req, res) => {
+  await Post.deleteOne({ _id: req.params.postId });
 
-const deleteSinglePost = async (req, res, next) => {
-  try {
-    await Post.deleteOne({ _id: req.params.postId });
-
-    return res.status(200).json({
-      msg: `deleting post ${req.params.postId}...`,
-    });
-  } catch (err) {
-    next(err);
-  }
-};
+  return res.status(200).json({
+    msg: `deleting post ${req.params.postId}...`,
+  });
+});
 
 module.exports = {
   getAllPosts,
