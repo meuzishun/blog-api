e367226de385e7af151f0a42d212390db7270358
Andrew Smith <meuzishun@gmail.com>
2023-07-15 15:35:06 -0400

    Add rate limiting

--- app.js
+++ app.js
@@ -10,11 +10,15 @@ const routes = require('./routes/index');
 const { errorHandler } = require('./middleware/errorHandler');
 const compression = require('compression');
 const helmet = require('helmet');
+const RateLimit = require('express-rate-limit');
+const limiter = RateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 20,
+});
 
 const app = express();
 
-// TODO: Setup cors configuration (https://www.npmjs.com/package/cors)
-
+app.use(limiter);
 app.use(compression());
 app.use(helmet());
 // app.use(
@@ -22,7 +26,7 @@ app.use(helmet());
 //     crossOriginResourcePolicy: false,
 //   })
 // );
-app.use(cors());
+app.use(cors()); // TODO: Setup cors configuration (https://www.npmjs.com/package/cors)
 app.use(passport.initialize());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
